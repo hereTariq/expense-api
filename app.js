8bea473eb6a85f3283ab85ca810acf8f3c53bf71
Mohammad Tariq <fk68790@gmail.com>
2022-12-22 19:22:18 +0530

    minor changes

--- app.js
+++ app.js
@@ -11,8 +11,13 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/expense', expenseRoutes);
 
-mongoose.connect(process.env.URI);
-
-app.listen(PORT, () =>
-    console.log('server is running on http://localhost:' + PORT)
-);
+mongoose.set('strictQuery', false);
+mongoose
+    .connect(process.env.URI)
+    .then((result) => {
+        console.log('mongodb connected');
+        app.listen(PORT, () =>
+            console.log('server is running on http://localhost:' + PORT)
+        );
+    })
+    .catch((err) => console.log(err));
